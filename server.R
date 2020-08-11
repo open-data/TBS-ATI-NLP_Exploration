c27caf40375234bfc17a307eca06b717b1717efe
CSBurkett <cburkett@gmail.com>
2020-08-11 11:12:14 -0400

    Functional shiny app showing TF-IDF, topic modeling, n-grams and EDA

---
+++ server.R
@@ -0,0 +1,297 @@
+# options(warn = -1) # For suppressing plotly warnings
+library(shiny)
+library(shinydashboard)
+
+server <- function(input, output, session){
+  
+  
+  ### ----------------- ###
+  ### Reactive elements ###
+  ### ----------------- ###
+  rv = reactiveValues()
+  rv$stops = stop_words
+  
+  rawDataSet = reactive({
+    req(c(input$dataSource))
+    
+    read_csv(paste0(input$dataSource, ".csv")) %>% clean_ati()
+      
+  })
+
+  # Figure out which variables are dates, factors, numeric, and binary
+  varClasses = reactive({    sapply(sapply(rawDataSet(), class), "[[", 1)    })
+  
+  dateList   = reactive({    names(varClasses())[varClasses() %in% c("Date", "POSIXct")]    })
+  factorList = reactive({    names(varClasses())[varClasses() %in% c("factor", "character")]    })
+  numberList = reactive({    names(varClasses())[varClasses() %in% c("numeric", "integer", "difftime")]    })
+  binaryList = reactive({    names(varClasses())[varClasses() %in% c("logical")]    })
+  
+  # Filtered data set
+  filterData = reactive({
+    req(input$filterDataSample, input$dept)
+    
+    rawDataSet() %>%
+      {if(input$filterDataSample != "All") filter(., owner == input$dept) else .}
+  })
+  
+  
+  # Summary descriptive stats
+  summaryStats = reactive({
+    req(input$tabs == "Bivariate", input$multivarX %in% names(filterData()), input$multivarY %in% names(filterData()))
+    
+    filterData() %>% select(input$multivarX, input$multivarY) %>% get_summary_stats()
+  })
+    
+  nTotal = reactive(  nrow(filterData())   ) # Printed text
+  ownersTop9 = reactive({rawDataSet() %>% group_by(owner) %>% count() %>% ungroup() %>% top_n(9, n) %>% pull(owner)   })
+  
+  ## Update custom stop word list
+  stopWordList = observeEvent(input$goStop, {
+    newWords = strsplit(input$stopWordInput, ",") %>% 
+      unlist() %>% 
+      trimws()
+    if(length(newWords) == 0) return(stop_words)
+    
+    rv$stops = data.frame(word = newWords, lexicon = "CUSTOM") %>%
+      bind_rows(stop_words)
+  })
+  
+  ## Make N-grams
+  unigram = reactive({
+    rawDataSet() %>% 
+      filter(owner %in% ownersTop9()) %>%
+      count_unigrams(rv$stops) %>%
+      group_by(owner) %>% 
+      count(word)
+  })
+  
+  bigram = reactive({
+    rawDataSet() %>% 
+      filter(owner %in% ownersTop9()) %>%
+      count_bigrams(rv$stops)
+  })
+  
+  trigram = reactive({
+    rawDataSet() %>% 
+      filter(owner %in% ownersTop9()) %>%
+      count_trigrams(rv$stops)
+  })
+  
+  # tetragram = reactive({
+  #   rawDataSet() %>% 
+  #     filter(owner %in% ownersTop9()) %>%
+  #     count_tetragrams(rv$stops)
+  # })
+  
+  word_cors = reactive({
+    req(input$dept, input$bigramN, filterData())
+    
+    thisDept = filterData() %>% filter(owner == input$dept)
+    topWords = unigram() %>% filter(owner == input$dept) %>% filter(n >= input$bigramN) %>% pull(word)
+    
+    thisDept %>%
+      count_unigrams(rv$stops) %>%
+      filter(word %in% topWords) %>%
+      pairwise_cor(word, ID, sort = TRUE)
+  })
+  
+  dtm = reactive({
+    req(input$dept, filterData())
+    
+    filterData() %>% 
+      filter(owner == input$dept) %>%
+      count_unigrams(rv$stops) %>%
+      group_by(ID, owner) %>% 
+      count(word) %>%
+      cast_dtm(ID, word, n)
+  })
+  
+ 
+  
+  ### -------------- ###
+  ### Input elements ###
+  ### -------------- ###
+  
+  ## Sidebar
+  ## Uni tab
+  output$selectFac <- renderUI(  selectInput("facVar", "Factor Variable:",   choices = intersect(factorList(), names(rawDataSet())))  )
+  output$selectNum <- renderUI(  selectInput("numVar", "Numeric Variable:",  choices = intersect(c(numberList(), dateList()), names(rawDataSet())))  )
+  output$selectDat <- renderUI(  selectInput("datVar", "DateTime Variable:", choices = intersect(dateList(), names(rawDataSet())))  )
+  output$selectBin <- renderUI(  selectInput("binVar", "Binary Variable:",   choices = intersect(binaryList(), names(rawDataSet())))  )
+  
+  ## Bi tab
+  output$selectMultivarX <- renderUI(    selectInput("multivarX", "X Variable:",  choices = names(rawDataSet()))   )
+  output$selectMultivarY <- renderUI(    selectInput("multivarY", "Y Variable:",  choices = intersect(c(factorList(), numberList()), names(rawDataSet())))  )
+  output$selectMultivarG <- renderUI(    selectInput("multivarG", "Grouping Variable (Boxplot & Scatter):",   choices = c("NONE", intersect(c(factorList(), binaryList()), names(rawDataSet()))))  )
+  
+  observeEvent(input$switchVarsBi, {
+    tempX = input$multivarX
+    tempY = input$multivarY
+    
+    output$selectMultivarX <- renderUI(    selectInput("multivarX", "X Variable:",  choices = names(rawDataSet()), selected = tempY)   )
+    output$selectMultivarY <- renderUI({
+      selectInput("multivarY", "Y Variable:",  choices = intersect(c(factorList(), numberList()), names(rawDataSet())), selected = tempX)
+    })
+  })
+  
+  ## Bigram tab
+  output$deptOut = renderUI(   selectInput("dept", "Department", ownersTop9())   )
+  
+  
+  ### --------------- ###
+  ### Output elements ###
+  ### --------------- ###
+  
+  ### Tables ###
+  ### ------ ###
+  
+  output$tableBigramN = DT::renderDataTable(    DT::datatable(filter(bigram(), owner == input$dept), options = list(pageLength = 20))   )
+  output$tableBigramCorr = DT::renderDataTable(    DT::datatable(filter(word_cors(), correlation > input$bigramCorrMin), options = list(pageLength = 18))   )
+  
+  
+  ### Plots ###
+  ### ----- ###
+  
+  ## Uni tab
+  output$plotBar  = renderPlot({
+    req(input$facVar %in% names(rawDataSet()))
+    
+    filterData() %>% select(input$facVar) %>%
+      gg_count(type= "Bar", x.lab = input$facVar, maxLevels = input$maxBarsUni, maxLabelL = input$maxLabelUni, 
+               iLog = input$iLogBarUni, iLabel = input$iLabelBarUni, iPareto = input$iParetoUni, iRotateX = input$iRotateXUni)
+  }, bg= "transparent")
+  
+  output$plotHist = renderPlot({
+    req(input$numVar %in% names(rawDataSet()))
+    
+    filterData() %>% select(input$numVar) %>%
+      gg_hist(x.lab = input$numVar, myBins = input$numBinsHistUni, iLog = input$iLogHistUni)
+  }, bg= "transparent")
+
+  output$plotWC = renderPlot({
+    req(input$dept, unigram())
+    
+    ati_wc = unigram() %>%
+      filter(owner == input$dept) %>%
+      filter(n >= input$bigramN)
+    
+    wordcloud(ati_wc$word, ati_wc$n, max.words = 100, random.order=FALSE, rot.per=0.35, colors=brewer.pal(8, "Dark2"))
+  }, bg= "transparent")
+
+  
+  ## Bi tab
+  output$plotMulti = renderPlot({
+    req(input$multivarX %in% names(filterData()), input$multivarY %in% names(filterData())) # , input$tabs == "Bivariate"
+    
+    filterData() %>%
+      {if(input$multivarG == "NONE") select(., input$multivarX, input$multivarY) else select(., input$multivarX, input$multivarY, input$multivarG)} %>%
+      make_multi_plot(input$catXcatType, input$maxLevelsMulti, input$maxGroupsMulti, input$maxLabelMulti, input$cutoffMulti, input$iKeepFac, input$iLogMulti, 
+                      input$iRotateXMulti, input$iFreqPoly, input$numBinsMulti, input$iLinearMulti, input$iXYMulti, input$iJitterMulti)
+  }, bg= "transparent")
+  
+  ## TF-IDF tab
+  output$plotTFIDF = renderPlot({
+    req(input$nGramN, input$tfTopN)
+    
+    if(input$nGramN == 1) dd = unigram() %>% bind_tf_idf(word, owner, n)
+    
+    if(input$nGramN == 2) {
+      dd = bigram() %>% 
+        unite(bigram, word1, word2, sep = " ") %>%
+        count(owner, bigram, wt = n) %>%
+        bind_tf_idf(bigram, owner, n)
+    }
+    
+    if(input$nGramN == 3) {
+      dd = trigram() %>% 
+        unite(trigram, word1, word2, word3, sep = " ") %>%
+        count(owner, trigram, wt = n) %>%
+        bind_tf_idf(trigram, owner, n)
+    }
+    
+    dd %>%
+      arrange(desc(tf_idf)) %>%
+      visualize_tfidf(input$tfTopN)  
+    
+  }, bg= "transparent")
+  
+  ## Bigram tabs
+  output$plotBigramN = renderPlot({
+    
+    bigram() %>%
+      filter(owner == input$dept) %>%
+      ungroup() %>%
+      select(-owner) %>%
+      filter(n > input$bigramN) %>%
+      visualize_bigrams()
+
+  }, bg= "transparent")
+  
+  output$plotBigramCorr = renderPlot({
+    
+    word_cors() %>%
+      filter(correlation > input$bigramCorrMin) %>%
+      graph_from_data_frame() %>%
+      ggraph(layout = "fr") +
+      geom_edge_link(aes(edge_alpha = correlation), show.legend = FALSE) +
+      geom_node_point(color = "lightblue", size = 5) +
+      geom_node_text(aes(label = name), repel = TRUE) +
+      theme_void()
+    
+  }, bg= "transparent")
+  
+  ## Topic Modeling tab
+  output$plotTopics = renderPlot({
+    req(dtm())
+    
+    dtm() %>% 
+      LDA(k = input$topicN, control = list(seed = 1234)) %>%
+      tidy(matrix = "beta") %>%
+      group_by(topic) %>%
+      top_n(input$topicTopN, beta) %>%
+      ungroup() %>%
+      arrange(topic, -beta) %>%
+      mutate(term = reorder_within(term, beta, topic)) %>%
+      ggplot(aes(term, beta, fill = factor(topic))) +
+      geom_col(show.legend = FALSE) +
+      facet_wrap(~ topic, scales = "free") +
+      coord_flip() +
+      scale_x_reordered() +
+      theme(axis.text = element_text(size = 12))
+    
+  }, bg= "transparent")
+  
+  
+  ### Summary Text ###
+  ### ------------ ###
+
+  ## Sidebar
+  output$dataNtotal  = renderText(  nTotal()  )
+  
+  ## Bi tab
+  output$summStatsAvgX  = renderText(   make_summ_text(summaryStats(), "avg",  input$multivarX, input$tabs)   )
+  output$summStatsSdX   = renderText(   make_summ_text(summaryStats(), "sd",   input$multivarX, input$tabs)   )
+  output$summStatsAvgY  = renderText(   make_summ_text(summaryStats(), "avg",  input$multivarY, input$tabs)   )
+  output$summStatsSdY   = renderText(   make_summ_text(summaryStats(), "sd",   input$multivarY, input$tabs)   )
+  
+  ### Gauges ###
+  ### ------ ###
+  
+  ## Univariate tab
+  output$gaugeFac = renderGauge({   req(input$facVar %in% names(rawDataSet())); render_gauge(input$facVar, filterData())   })
+  output$gaugeNum = renderGauge({   req(input$numVar %in% names(rawDataSet())); render_gauge(input$numVar, filterData())   })
+  
+  ## Bivariate tab
+  output$gaugeBiComp = renderGauge({
+    req(input$multivarX %in% names(filterData()), input$multivarY %in% names(filterData()), input$tabs == "Bivariate")
+    
+    filterData() %>%
+      select(input$multivarX, input$multivarY) %>% 
+      complete.cases() %>% mean() %>% make_gauge()
+  })
+  
+  # output$testText1 = renderPrint(str(word_cors())) # Testing - remove
+  # session$onSessionEnded(stopApp) # Uncomment to have R stop on browser close
+  
+} # END server
+
---
+++ ui.R
@@ -0,0 +1,221 @@
+library(shiny)
+library(shinydashboard)
+
+webPageTitle = "TBS-ATI NLP Exploration"
+
+tweaks <- 
+  list(tags$head(tags$style(HTML("
+                                 .multicol {
+                                   height: auto;
+                                   -webkit-column-count: 3; /* Chrome, Safari, Opera */ 
+                                   -moz-column-count: 3;    /* Firefox */ 
+                                   column-count: 3; 
+                                   -moz-column-fill: auto;
+                                   -column-fill: auto;
+                                 } 
+                                 ")) 
+  ), 
+  tags$head(tags$script(HTML("$(document).on('click', '.needed', function () {
+                                Shiny.onInputChange('last_btn',this.id);
+                             });")))
+  )
+
+UIFacColWidth = 4
+UINumColWidth = 4
+UIWcColWidth = 4
+
+header = dashboardHeader(
+  title = span(
+     HTML(paste(webPageTitle, "<sup> BETA </sup>"))) # <sub>v0.1</sub>
+  , titleWidth = 350
+  , dropdownMenu(type = "messages", icon = icon("info-circle"), 
+                 messageItem(from = "Nigerian Prince", 
+                             message = tags$div("Investment Opportunity"), icon = icon(NULL)   )
+  ) # END dropdown
+  
+) # END Header 
+
+
+sidebar = dashboardSidebar(
+  tags$head(tags$style(HTML('
+                            /* menu */
+                            .skin-purple .sidebar-menu > li.active > a {
+                            background-color: #545096;
+                            }
+                            #                           '))), # https://www.quackit.com/css/css_color_codes.cfm
+  
+  # width = 450,
+  
+  sidebarMenu(id= "menu"
+              , selectInput("dataSource", "Choose data source:", choices = sub(".csv", "", list.files(pattern = "csv")))
+
+              , fluidRow(
+                column(6, selectInput("filterDataSample", "Sample", c("Dept", "All"), "Dept"))
+                ,column(6, h3("N = ", textOutput("dataNtotal", inline= T)))
+              )
+              
+              , fluidRow(column(12, uiOutput("deptOut")))
+              , fluidRow(column(8, numericInput("bigramN", "Top N for Bigrams", 20, 5, 50, 5)))
+              
+              , fluidRow(column(12, textInput("stopWordInput", "Input custom stop words (CSV):")))
+              , fluidRow(column(12, actionButton("goStop", "Enhance!", icon = icon("laptop"))))
+              
+              # , verbatimTextOutput("testText1") # Testing - remove
+), width = 300) # END Sidebar 
+
+body = dashboardBody(
+  tags$head(
+    tags$style(
+      HTML(".shiny-notification {position: fixed; top: calc(80%); left: calc(50%); font-size: 32px;}")
+    )), 
+  
+  tabPanel(""
+           , tabsetPanel(id = "tabs",
+                         tabPanel("Univariate",
+                                  fluidPage(
+                                    fluidRow(
+                                       column(UIFacColWidth, uiOutput("selectFac") )
+                                      ,column(UINumColWidth, uiOutput("selectNum") )
+                                    ) # END row
+                                    
+                                    ,fluidRow(
+                                      column(UIFacColWidth, 
+                                             fluidRow(
+                                                column(3, numericInput("maxLabelUni", "Max Length", 20, 6, 40, 2))
+                                               ,column(3, numericInput("maxBarsUni", "Max Bars", 8, 2, 40, 2))
+                                               
+                                               ,column(3, div(style = "margin-top:-0em", checkboxInput("iParetoUni", "Pareto order",  value= TRUE))
+                                                        , div(style = "margin-top:-1em", checkboxInput("iLogBarUni", "log transform", value= FALSE)))
+                                               
+                                               ,column(3, div(style = "margin-top:-0em", checkboxInput("iRotateXUni", "Rotate labels", value= FALSE))
+                                                        , div(style = "margin-top:-1em", checkboxInput("iLabelBarUni", "Add labels", value= FALSE)))
+                                             ) # END row
+                                      ) # END col
+                                      
+                                      ,column(UINumColWidth, 
+                                              fluidRow(
+                                                column(6, numericInput("numBinsHistUni", "Hist Bins", 30, 6, 50, 4))
+                                                ,column(6, checkboxInput("iLogHistUni", "log transform", value= FALSE))
+                                              )
+                                      ) # END col
+                                      
+                                    ) # END row above graphs
+                                    
+                                    ,fluidRow(
+                                       column(UIFacColWidth, plotOutput("plotBar", height = 600))
+                                      ,column(UINumColWidth, plotOutput("plotHist", height = 600))
+                                      ,column(UIWcColWidth, plotOutput("plotWC", height = 600))
+                                    ) # END row
+                                    
+                                    ,fluidRow(
+                                       column(UIFacColWidth, h3("Percent Complete", gaugeOutput("gaugeFac")))
+                                      ,column(UINumColWidth, h3("Percent Complete", gaugeOutput("gaugeNum")))
+                                    ) # END row
+                                    
+                                    ) # END FluidPage
+                         ) # END Tab Uni
+                         
+                         , tabPanel("Bivariate",
+                                  fluidPage(
+                                    fluidRow(
+                                      column(10 
+                                             ,fluidRow(
+                                               column(3, uiOutput("selectMultivarX"))
+                                               ,column(1, br(), actionButton("switchVarsBi", "", icon = icon("exchange")))
+                                               ,column(3, uiOutput("selectMultivarY"))
+                                               ,column(3, uiOutput("selectMultivarG"))
+                                             )
+                                             
+                                             ,fluidRow(
+                                               column(3
+                                                      , h5("Avg:  ",   textOutput("summStatsAvgX",  inline = T))
+                                                      , h5("SD:   ",   textOutput("summStatsSdX",   inline = T))
+                                                      )
+                                               ,column(1, h5(""))
+                                               ,column(3
+                                                       , h5("Avg:  ",   textOutput("summStatsAvgY",  inline = T))
+                                                       , h5("SD:   ",   textOutput("summStatsSdY",   inline = T))
+                                                       )
+                                               ,column(3
+                                                       , radioButtons("catXcatType", "Plot Type", c("Bar", "Circle", "Tile"), inline = T)
+                                                       , numericInput("maxLabelMulti", "Max Label Length", 20, 6, 40, 2)
+                                                       )
+                                               ) # END row
+                                             ) # END col
+                                      ,column(2, h3("Complete Data", gaugeOutput("gaugeBiComp")))
+                                    ) # END row
+                                    
+                                     ,fluidRow(
+                                        column(1, numericInput("maxLevelsMulti", "Max Levels", 8, 2, 40, 2))
+                                       ,column(1, numericInput("maxGroupsMulti", "Max Groups", 4, 1, 10, 1))
+                                       ,column(1, div(style = "margin-top:-0em", checkboxInput("iKeepFac", "Keep Levels",  value= TRUE))
+                                                , div(style = "margin-top:-1em", checkboxInput("iLogMulti", "log transform", value= FALSE)))
+                                       ,column(1, div(style = "margin-top:-0em", checkboxInput("iFreqPoly", "Polygon", value= FALSE))
+                                                , div(style = "margin-top:-1em", checkboxInput("iRotateXMulti", "Rotate labels", value= FALSE)))
+                                       ,column(1, numericInput("numBinsMulti", "Hist Bins", 30, 6, 50, 4))
+                                       ,column(1, numericInput("cutoffMulti", "Numeric Cutoff", 100000, 0, 1000000, 100))
+                                       ,column(1, div(style = "margin-top:-0em", checkboxInput("iLinearMulti", "Linear fit",  value= FALSE))
+                                                , div(style = "margin-top:-1em", checkboxInput("iXYMulti", "Y = X", value= FALSE)))
+                                       ,column(1, checkboxInput("iJitterMulti", "Jitter", value= FALSE))
+                                    )
+                                    
+                                    ,fluidRow(plotOutput("plotMulti", height = 600))
+                                    
+                                  ) # END FluidPage
+                                  
+                         ) # END Tab Multi
+                         
+                         
+                         , tabPanel("TF-IDF",
+                                    fluidPage(
+                                      fluidRow(
+                                        column(1, numericInput("nGramN", "N-grams", 1, 1, 3, 1))
+                                        ,column(1, numericInput("tfTopN", "Top N", 10, 6, 20, 2))
+                                      ) # END row
+                                      
+                                      ,plotOutput("plotTFIDF", height = 800)
+                                      
+                                    ) # END FluidPage
+                                    
+                         ) # END Tab TF-IDF
+                         
+                         , tabPanel("Bigrams - N",
+                                    fluidPage(
+                                      column(4, DT::dataTableOutput("tableBigramN"))
+                                      ,column(8, plotOutput("plotBigramN", height = 800))
+                                      
+                                    ) # END FluidPage
+                                    
+                         ) # END Tab Bigrams
+                        
+                         , tabPanel("Bigrams - Corr",
+                                    fluidPage(
+                                      column(4
+                                             , numericInput("bigramCorrMin", "Min Corr", 0.3, -0.5, 1, 0.05)
+                                             , DT::dataTableOutput("tableBigramCorr"))
+                                      ,column(8, plotOutput("plotBigramCorr", height = 800))
+                                      
+                                    ) # END FluidPage
+                                    
+                         ) # END Tab Bigrams
+                         
+                         , tabPanel("Topic Modeling",
+                                    fluidPage(
+                                      fluidRow(
+                                        column(1, numericInput("topicN", "Topics", 9, 4, 16, 1))
+                                        ,column(1, numericInput("topicTopN", "Top N", 10, 6, 20, 2))
+                                      ) # END row
+                                      
+                                      ,plotOutput("plotTopics", height = 800)
+                                      
+                                    ) # END FluidPage
+                                    
+                         ) # END Tab Topic
+                         
+                         , selected = "Topic Modeling"
+                         
+                         ) # END tabsetPanel
+           ) # END tabPanel MAIN
+) # END Body
+
+dashboardPage(header, sidebar, body, skin = "purple", title = HTML(webPageTitle))
